5e816012c1bf4d09f4ee974c0e903a7a9ebc1c42
Flleeppyy <fleeppyybusiness@gmail.com>
2022-02-11 09:39:15 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// Create a simple http server
+// and when a request"s Host header is equal to "hehhoo.fleepy.tv", fetch a webpage and proxy it to the user
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const host = req.headers.host;
+  const cookies = req.headers.cookie;
+  if (host === "k24a1.fleepy.tv") {
+    const urlObj = url.parse(req.url);
+    const path = urlObj.pathname;
+    const headers = {
+      ...req.headers,
+      "Host": "k24a1.42web.io",
+    }
+    headers.cookie = cookies + "; __test=325c6b5dfce19c75b820329c6056d1ea";
+    headers.host = "k24a1.42web.io";
+
+    // http.request
+    const options = {
+      host: "k24a1.42web.io",
+      port: 80,
+      path: path,
+      method: req.method,
+      headers: headers,
+    };
+
+    const proxyReq = http.request(options, (proxyRes) => {
+      Object.keys(proxyRes.headers).forEach((key) => {
+        res.setHeader(key, proxyRes.headers[key]);
+      });
+      proxyRes.pipe(res);
+    });
+
+    req.headers = headers;
+
+    req.pipe(proxyReq);
+    // fetch
+    // fetch({
+    //   "url": "http://185.27.134.133/" + path,
+    //   "headers": headers
+    // }).then(function(response) {
+    //   res.writeHead(response.status, {
+    //     ...response.headers,
+    //   });
+    //   res.end(response);
+    // }).catch(function(err) {
+    //   res.writeHead(response.status, {
+    //     ...response.headers,
+    //   });
+    //   res.end(response);
+    // });
+  }
+});
+
+server.listen(8089);
+        
